d0a216b1a118433acc7398f55ae22d923cfe14b2
Chris Minnick <chris@minnick.com>
2022-03-02 13:22:45 -0800

    lab11

--- vue-training/conduit-app/ui/src/components/ArticleList.vue
+++ vue-training/conduit-app/ui/src/components/ArticleList.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <div v-if="articles.length === 0" class="article-preview">
-      No articles are here... yet.
+      No articles are here ..yet.
     </div>
     <ArticlePreview
       v-for="(article, index) in articles"
       :article="article"
       :key="article.title + index"
+      :style="{ fontSize: articleFontSize + 'em' }"
+      v-on:enlarge-text="articleFontSize += $event"
+      v-on:shrink-text="articleFontSize === 1 ? 1 : (articleFontSize -= $event)"
     />
   </div>
 </template>
-
 <script>
 import ArticlePreview from './ArticlePreview.vue';
 import articles from '../json/articles.json';
@@ -23,7 +25,10 @@ export default {
   data() {
     return {
       articles: articles,
+      articleFontSize: 1,
     };
   },
 };
 </script>
+
+<style></style>
--- vue-training/conduit-app/ui/src/components/ArticlePreview.vue
+++ vue-training/conduit-app/ui/src/components/ArticlePreview.vue
@@ -12,20 +12,19 @@
       <i class="ion-heart"></i>
       <span class="counter"> {{ this.favoritesCount }} </span>
     </button>
+    <button style="margin: 3px" v-on:click="$emit('enlarge-text', 0.1)">
+      Enlarge Text
+    </button>
+    <button style="margin: 3px" v-on:click="$emit('shrink-text', 0.1)">
+      Shrink Text
+    </button>
     <p v-text="article.description" />
-    <span><a :href="articleLink.slug">Read more...</a></span>
+    <span><a :href="'article.slug'">Read more...</a></span>
   </div>
 </template>
-
 <script>
 export default {
   name: 'ArticlePreview',
-  data: function () {
-    return {
-      favorited: false,
-      favoritesCount: 0,
-    };
-  },
   props: {
     article: { type: Object, required: true },
   },
@@ -36,8 +35,14 @@ export default {
       };
     },
   },
+  data: function () {
+    return {
+      favorited: false,
+      favoritesCount: 0,
+    };
+  },
   methods: {
-    toggleFavorite() {
+    toggleFavorite: function () {
       this.favorited = !this.favorited;
       this.favoritesCount++;
       console.log('favorited = ' + this.favorited);
