b177fb7c5f190d145a196009d2b2647c81f78d9c
Chris Minnick <chris@minnick.com>
2022-03-13 13:07:18 -0700

    starting lab29

---
+++ conduit-app/api/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const articles = require('./routes/api/mongoRouter');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.json());
+const db = require('./model');
+console.log(db.url);
+
+app.get('/', (req, res) => {
+  res.send('This is the home page');
+});
+
+async function connect() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+  console.log('database is created.');
+}
+try {
+  connect();
+} catch (e) {
+  console.error(e);
+}
+
+const server = app.listen(process.env.port || 3000, () => {
+  console.log('mongoApp Listening on port ' + server.address().port);
+});
