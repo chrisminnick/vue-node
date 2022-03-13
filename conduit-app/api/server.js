50bfeb7e5c075db7c03a0dd0f8d27ecd3bfd8609
Chris Minnick <chris@minnick.com>
2022-03-13 14:11:45 -0700

    finished lab29

--- conduit-app/api/server.js
+++ conduit-app/api/server.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
+require('dotenv').config(); // new
 const express = require('express');
-const app = express();
+//const articles = require('./routes/api/articles')
 const articles = require('./routes/api/mongoRouter');
+const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -9,23 +10,23 @@ const mongoose = require('mongoose');
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
+app.use('/api', articles); // change this
+
 const db = require('./model');
 console.log(db.url);
+
+async function connect() {
+  await mongoose.connect(db.url);
+  //  await mongoose.connect("mongodb://localhost:27017/test")
+  console.log('database is created');
+}
+
+connect();
 
 app.get('/', (req, res) => {
   res.send('This is the home page');
 });
 
-async function connect() {
-  await mongoose.connect('mongodb://localhost:27017/test');
-  console.log('database is created.');
-}
-try {
-  connect();
-} catch (e) {
-  console.error(e);
-}
-
 const server = app.listen(process.env.port || 3000, () => {
-  console.log('mongoApp Listening on port ' + server.address().port);
+  console.log('Listening on port - mongoApp ' + server.address().port);
 });
