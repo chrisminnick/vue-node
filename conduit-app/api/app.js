34396cbe8e08a8a980496b8c0c5f29d7ec14ca15
Chris Minnick <chris@minnick.com>
2022-03-13 06:19:15 -0700

    lab19

--- conduit-app/api/app.js
+++ conduit-app/api/app.js
@@ -2,8 +2,25 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('http://localhost:3000 GET method');
+  res.send('This is the home page');
 });
+
+app.get('/articles', (req, res) => {
+  res.send('http://localhost:3000/articles GET method');
+});
+
+app.post('/articles', (req, res) => {
+  res.send('http://localhost:3000/articles POST method');
+});
+
+app.put('/articles', (req, res) => {
+  res.send('http://localhost:3000/articles PUT method');
+});
+
+app.delete('/articles', (req, res) => {
+  res.send('http://localhost:3000/articles DELETE method');
+});
+
 const server = app.listen(process.env.port || 3000, () => {
   console.log('Listening on port ' + server.address().port);
 });
