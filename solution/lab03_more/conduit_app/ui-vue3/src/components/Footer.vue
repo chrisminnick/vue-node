f71cd3c3e432b23b0a04fb726a35a44aa6414a66
Chris Minnick <chris@minnick.com>
2023-05-01 12:59:50 -0700

    lab3

---
+++ solution/lab03_more/conduit_app/ui-vue3/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <div class="container">
+      conduit
+      <span class="attribution"> Code &amp; design licensed under MIT. </span>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
