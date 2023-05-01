416f72d2c2eb21948c7f114cdf6a208d01d5a4ec
Chris Minnick <chris@minnick.com>
2023-05-01 13:40:15 -0700

    lab04

---
+++ conduit-vue3/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer>
+    <div class="container">
+      conduit
+      <span class="attribution"> Code &amp; design licensed under MIT. </span>
+    </div>
+  </footer>
+</template>
+<script setup lang="ts"></script>
